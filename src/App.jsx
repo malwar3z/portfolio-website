46c2ee3d4867259dfa8a416ab4aff439df2d286a
Matias Nicolas Alvarez <matias.n.alvarez@avanade.com>
2022-04-29 21:20:35 +0200

    added airbn eslint config + basic folder structure + sass

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './views/Home';
+
+function App() {
+  // useEffect(() => {
+  //   document.title = "Alvarez Matias"
+  // },[]);
+
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
